19da8441ca1149b936cf408bc094a1085dcf2dc8
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-04-13 09:21:46 -0500

    feat: :art: Parte 2: tabla y detallitos
    parte 2 de la actividad

--- main.js
+++ main.js
@@ -37,11 +37,36 @@ class lapiz{
      getMaterial(){
         return this.material;
      }
+
+    obtener(){
+      obj.color = color.value;
+      obj.dimencion = parseInt(dimension.value);
+      marca.forEach((elem) => {
+          if (elem.checked) {
+            obj.setMarca({ marca: elem.value });
+          }
+      });
+      borrador.forEach((elem) => {
+          if (elem.checked) {
+            obj.borrador = Boolean(elem.value);
+            console.log(borrador);
+          }
+      });
+  
+      material.forEach((elem) => {
+        if (elem.checked) {
+          obj.material = elem.value;
+        }
+    });  
+    }
  }
+
+ let myFormData = document.querySelector("#myFormData");
+ let myTablitaData = document.querySelector("#myTablitaData") 
 
  let obj = undefined;
  let color = document.querySelector(`[name="color"]`);
- let descripcion = document.querySelector(`[name="dimension"]`);
+ let dimension = document.querySelector(`[name="dimension"]`);
  let marca = document.querySelectorAll(`[name="marca"]`);
  let borrador = document.querySelectorAll(`[name="borrador"]`);
  let material = document.querySelectorAll(`[name="material"]`);
@@ -50,7 +75,7 @@ class lapiz{
     obj = new lapiz({});
     console.log(obj);
     color.value = obj.color;
-    descripcion.value = obj.dimencion;
+    dimension.value = obj.dimencion;
     marca.forEach((radio) => {
         if (radio.value === obj.getMarca()) {
           radio.checked = true;
@@ -68,11 +93,36 @@ class lapiz{
     })
  })
 
+ myFormData.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
  
+    obj.obtener();
+
+    myTablitaData.classList.remove("div_hide");
+
+    let myTbodyData = document.querySelector("#myTbodyData");
+    
+    myTbodyData.insertAdjacentHTML("beforeend", `
+    <tr id="atributos">
+      <td style="color:${obj.color}">${obj.color}</td>
+      <td>${obj.dimencion} cm</td>
+      <td>${obj.getMarca()}</td>
+      <td>${obj.borrador ? "Tiene" : "No Tiene"}</td>
+      <td>${obj.material}</td>
+
+    </tr>
+    `)
+   
+    console.log(data);
+    console.log(obj);
+   
+ })
+
+
  function showValue(newValue) {
     document.getElementById("rangeValue").innerHTML = newValue;
  }
 
- let myString = "true";
-let myBoolean = Boolean(myString);
+
 
